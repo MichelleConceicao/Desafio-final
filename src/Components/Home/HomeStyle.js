0a455cefa03fd781ca27b03c5d5bbd02a5722b1d
MichelleConceição <liliandlentretenimento@gmail.com>
2022-10-04 21:44:09 -0300

    Home

--- src/Components/Home/HomeStyle.js
+++ src/Components/Home/HomeStyle.js
@@ -9,7 +9,7 @@ export const Div = styled.div`
   height: 50vh;
   align-items: center;
   justify-content: center;
-  @media only screen and (min-width: 360px) and (max-width: 800px) {
+  @media only (min-width: 360px) (max-width: 800px) {
     position: absolute;
     display: flex;
     width: 80%;
@@ -23,22 +23,20 @@ export const Div = styled.div`
 export const H1 = styled.h1`
   font-family: "Times New Roman";
   letter-spacing: 0.3vw;
-  display: flex;
   align-items: center;
-  justify-content: center;
-  width: 70%;
-  font-size: 80px;
-  height: 39vh;
+  width: 90%;
+  font-size: 320%;
+  height: 65vh;
 
-  @media only screen and (min-width: 360px) and (max-width: 800px) {
+  @media screen and (min-width: 360px) and (max-width: 800px) {
     font-family: "Times New Roman";
-    letter-spacing: 0.3vw;
-    align-items: center;
-    margin-bottom: 35vh;
+    letter-spacing: 0.4vw;
+    align-items: ;
+    margin-bottom: 40vh;
     width: 100%;
-    font-size: 32px;
     height: 30vh;
-  }
+    font-size: 220%;
+   
 `;
 
 export const Box = styled.div`
@@ -64,5 +62,52 @@ export const Ul = styled.ul`
   li:hover {
     cursor: pointer;
     color: whitesmoke;
+  }
+`;
+
+export const Div1 = styled.figure`
+  width: 100%;
+  height: 82%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-evenly;
+  img {
+    width: 250px;
+    height: 250px;
+    border-radius: 40%;
+    object-fit: cover;
+    cursor: pointer;
+  }
+`;
+
+export const Div2 = styled.div`
+  position: absolute;
+  display: flex;
+  align-items: center;
+  justify-content: space-evenly;
+  width: 50%;
+  height: 30vh;
+  top: 70vh;
+  left: 25%;
+
+  @media only screen and (min-width: 360px) and (max-width: 800px) {
+    display: flex;
+    align-items: center;
+    flex-direction: wrap;
+    width: 70%;
+    height: 10vh;
+    top: 83vh;
+    left: 18.8%;
+  }
+`;
+
+export const Image = styled.img`
+  display: flex;
+  width: 100%;
+  height: 7vh;
+
+  @media only screen and (min-width: 360px) and (max-width: 800px) {
+    width: 88%;
   }
 `;
--- src/Components/Home/index.jsx
+++ src/Components/Home/index.jsx
@@ -1,6 +1,12 @@
 import React from "react";
 import * as S from "./HomeStyle.js";
+import Foto from "../Img/Michelle.png";
+import git from "../Img/git.png";
+import face from "../Img/face.png";
+import Linkedin from "../Img/in.png";
+
 import { Link } from "react-router-dom";
+
 
 export default function Home() {
   return (
@@ -22,6 +28,20 @@ export default function Home() {
             </Link>
           </S.Ul>
         </nav>
+        <S.Div1>
+          <img src={Foto} alt="Michelle" />
+        </S.Div1>
+        <S.Div2>
+          <a href="https://github.com/MichelleConceicao">
+            <S.Image src={git} />
+          </a>
+          <a href="https://www.linkedin.com/in/michelle-pereira-092283239/">
+            <S.Image src={Linkedin} />
+          </a>
+          <a href="https://www.facebook.com/michelle.daconceicaopereira">
+            <S.Image src={face} />
+          </a>
+        </S.Div2>
       </S.Box>
     </>
   );
